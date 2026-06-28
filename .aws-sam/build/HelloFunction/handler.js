d07a4f1b0004aa0a4a55ec64943c3846d525a547
Guruhbk <g.prasanth7@gmail.com>
2026-06-28 13:31:56 +0530

    init

---
+++ .aws-sam/build/HelloFunction/handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            message: "Hello World from GitHub Actions!!!",
+            requestId: event.requestContext?.requestId || null,
+            timestamp: new Date().toISOString()
+        })
+    };
+};
